2f21212a4ee1386a6837cd23f9c266ee2e2a6dc9
Ratchada Jududom <ratchada.jududom@gmail.com>
2021-06-21 15:55:37 -0700

    update content

--- wp-content/plugins/tag-manager-header-body-footer/include/settings.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
-<?php
-
-    global $wpdb;
-    $yydev_tags_table_name = $wpdb->prefix . "yydev_tagmanager"; // Database Table Name
-    $wp_options_name = "yydev_tagmanager_settings";
-
-?>
